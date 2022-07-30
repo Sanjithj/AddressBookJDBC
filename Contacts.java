14f2ab5634ffe0acba8ca29992fcf0277df1cbb3
Sanjithj <sanjithj591997@gmail.com>
2022-07-30 15:30:44 +0530

    Added: Contacts are added

---
+++ Contacts.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+public class Contacts {
+	    private String firstName;
+	    private String lastName;
+	    private String address;
+	    private String city;
+	    private String state;
+	    private int zip;
+	    private long phone;
+	    private String email;
+
+	    @Override
+	    public String toString()
+	    {
+	        return "First name is: "+firstName+"\nLast Name is: "+lastName+"\nAddress is: "+address+"\nCity name is: "+city+
+	                "\nState is: "+state+"\nZip is: "+zip+"\nPhone Number is: "+phone+"\nEmail Id is: "+email;
+	    }
+}
